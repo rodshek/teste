ff08603e3b68f59f0d6c99307ee34873e6d6dd9c
Rodrigo Rezende <rodrigojoselmartins@gmail.com>
2024-08-28 14:58:32 -0300

    first commit

--- app.py
+++ app.py
@@ -10,12 +10,12 @@ def index():
     suggested_numbers = None
     if request.method == 'POST':
         try:
-            number = int(request.form.get('number'))
+            number = int(request.form.get('number'))  # type: ignore
             if number < 0 or number > 36:
                 raise ValueError("Número inválido")
             suggested_numbers = get_suggested_numbers(number)
         except ValueError:
-            return render_template('index.html', error="Por favor, insira um número válido entre 0 e 36.")
+            return render_template('index.html', error="Number entre 0 e 36.")
 
     return render_template('index.html', suggested_numbers=suggested_numbers)
 
